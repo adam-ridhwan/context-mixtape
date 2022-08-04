7e0b1af19aeb2e1a011055da1379870424de1db7
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-08-04 15:37:38 -0400

    more refactorization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Controls } from './Components/Controls';
 import { SongList } from './Components/SongList';
+import { songs } from './Constants/Constants';
 import { MixtapeProvider } from './Contexts/MixtapeContext';
 import './index.css';
 
@@ -14,38 +15,5 @@ const App = () => {
     </div>
   );
 };
-
-const songs = [
-  {
-    artist: 'Smash Mouth',
-    genre: 'pop',
-    name: 'All Star',
-    year: 1999,
-  },
-  {
-    artist: 'Drake',
-    genre: 'rap',
-    name: 'Hotline Bling',
-    year: 2015,
-  },
-  {
-    artist: 'Lizzo',
-    genre: 'hip hop',
-    name: 'Juice',
-    year: 2019,
-  },
-  {
-    artist: 'Rick Astley',
-    genre: 'rock',
-    name: 'Never Gonna Give You Up',
-    year: 1987,
-  },
-  {
-    artist: '4 Non Blondes',
-    genre: 'rock',
-    name: "What's Up",
-    year: 1993,
-  },
-];
 
 export default App;
--- src/Constants/Constants.jsx
+++ src/Constants/Constants.jsx
@@ -1,2 +1,35 @@
+export const songs = [
+  {
+    artist: 'Smash Mouth',
+    genre: 'pop',
+    name: 'All Star',
+    year: 1999,
+  },
+  {
+    artist: 'Drake',
+    genre: 'rap',
+    name: 'Hotline Bling',
+    year: 2015,
+  },
+  {
+    artist: 'Lizzo',
+    genre: 'hip hop',
+    name: 'Juice',
+    year: 2019,
+  },
+  {
+    artist: 'Rick Astley',
+    genre: 'rock',
+    name: 'Never Gonna Give You Up',
+    year: 1987,
+  },
+  {
+    artist: '4 Non Blondes',
+    genre: 'rock',
+    name: "What's Up",
+    year: 1993,
+  },
+];
+
 export const INIT_GENRE = 'all';
 export const INIT_SORT_ORDER = 'ascending';
